03025eaad4c3c41cfdfb41bb6ffd2b47613608ce
Dmitry Stoletov <irvis@imega.ru>
2022-07-09 19:58:23 +0300

    fixed linter notifications
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

--- shop/discount.go
+++ shop/discount.go
@@ -2,13 +2,19 @@ package shop
 
 import "github.com/imega/mytheresa/domain"
 
+const (
+	discount30 = 30
+	discount15 = 15
+	hundred    = 100
+)
+
 type Discount struct{}
 
 func (d *Discount) Calc(product domain.Product) domain.Discount {
 	if product.Category == "boots" {
 		return domain.Discount{
 			Price: domain.Money{
-				Units:    product.Price.Units - (product.Price.Units * 30 / 100),
+				Units:    product.Price.Units - (product.Price.Units * discount30 / hundred),
 				Currency: product.Price.Currency,
 			},
 			Value: "30%",
@@ -18,7 +24,7 @@ func (d *Discount) Calc(product domain.Product) domain.Discount {
 	if product.SKU == "000003" {
 		return domain.Discount{
 			Price: domain.Money{
-				Units:    product.Price.Units - (product.Price.Units * 15 / 100),
+				Units:    product.Price.Units - (product.Price.Units * discount15 / hundred),
 				Currency: product.Price.Currency,
 			},
 			Value: "15%",

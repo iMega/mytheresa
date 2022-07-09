338bd1ff3b00390aa07f8ea5bc5698c08b52660d
Dmitry Stoletov <irvis@imega.ru>
2022-07-09 12:53:13 +0300

    added shop package with sketch-tests
    Signed-off-by: Dmitry Stoletov <irvis@imega.ru>

---
+++ shop/shop.go
@@ -0,0 +1,15 @@
+package shop
+
+import (
+	"context"
+
+	"github.com/imega/mytheresa/domain"
+)
+
+type Shop struct{}
+
+func Get(ctx context.Context, req domain.Request) [5]domain.Offer {
+	result := [5]domain.Offer{}
+
+	return result
+}
---
+++ shop/shop_test.go
@@ -0,0 +1,53 @@
+package shop
+
+import (
+	"context"
+	"reflect"
+	"testing"
+
+	"github.com/imega/mytheresa/domain"
+)
+
+func TestGet(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		req domain.Request
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want [5]domain.Offer
+	}{
+		{
+			name: "optimistic, get all products",
+		},
+		{
+			name: "get all products and apply the discount",
+		},
+		{
+			name: "get products filtered by category and apply the discount",
+		},
+		{
+			name: "discounts collide, the biggest discount must be applied",
+		},
+		{
+			name: "products in the boots category have a 30% discount",
+		},
+		{
+			name: "The product with sku=000003 has a 15% discount",
+		},
+		{
+			name: "get products filtered by priceLessThan 800 euro",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := Get(tt.args.ctx, tt.args.req)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Get() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
